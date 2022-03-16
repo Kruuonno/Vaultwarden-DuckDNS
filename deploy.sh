55a6cafcf3d59b4aa04bf8721d47f672d23bc0a2
Kruuonno <81293114+Kruuonno@users.noreply.github.com>
2022-03-16 02:39:49 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O https://raw.githubusercontent.com/Kruuonno/Vaultwarden-Local-Only/main/auto.sh | bash
+wget https://raw.githubusercontent.com/Kruuonno/Vaultwarden-Local-Only/main/auto.sh | bash
 chmod +x auto.sh
 sh ./auto.sh
