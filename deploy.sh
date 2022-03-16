40854fbec9755862e7346abcfe5622cfeb1a8380
Kruuonno <81293114+Kruuonno@users.noreply.github.com>
2022-03-16 02:48:40 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,33 @@
 git clone https://github.com/Kruuonno/Vaultwarden-Local-Only.git
 cd Vaultwarden-Local-Only
 chmod +x auto.sh
-./auto.sh
+
+read -p "Enter Duckdns Token : " token
+read -p "Enter Domain Name : " domain
+read -p "Enter Email : " email
+
+echo $token
+echo $domain
+echo $email
+
+token=$token
+domain=$domain
+email=$email
+
+sed -i "s/11111111111111111111111111/${token}/g" ./caddy.env
+
+sed -i "s/your/${domain}/g" ./caddy.env
+
+sed -i "s/email@gmail.com/${email}/g" ./.env
+
+sed -i "s/1111111111111111111/${token}/g" ./.env
+
+sed -i "s/your/${domain}/g" ./.env
+
+chmod u+x ./caddy
+
+cat ./.env
+cat ./caddy.env
+© 2022 GitHub, Inc.
+Terms
+P
