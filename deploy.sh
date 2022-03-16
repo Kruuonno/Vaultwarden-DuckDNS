c4e23536962c6f27c1b88ab129bff3dd273c0ce9
Kruuonno <81293114+Kruuonno@users.noreply.github.com>
2022-03-16 08:52:42 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-git clone https://github.com/Kruuonno/Vaultwarden-Local-Only.git
-chmod +x auto.sh
 sh ./auto.sh
+#!/bin/bash
+
+git clone https://github.com/Kruuonno/Vaultwarden-Local-Only.git
+wget -O auto.sh https://rebrand.ly/github-kruuonno-vault
+chmod +x auto.sh
+./auto.sh
