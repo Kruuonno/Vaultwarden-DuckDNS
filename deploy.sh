d3bee21c0f1653b0ad3640246d805efb65bc100b
Israel Garcia <81293114+Kruuonno@users.noreply.github.com>
2022-03-16 20:02:27 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ echo "Enter Your DuckDns Token"
 
 token=$(whiptail --inputbox "Enter Token Key" 10 30 3>&1 1>&2 2>&3)
 domain=$(whiptail --inputbox "Enter Your Duckdns Name Only" 10 30 3>&1 1>&2 2>&3)
-email=$(whiptail --inputbox "Enter Your Email" 10 30 3>&1 1>&2 2>&3)
+email=$(whiptail --inputbox "Enter Your Email" 10 30 3>&1 1>&2 2>&3); then
+echo "Running, exit status was $?."
 
 
 echo "$token"
