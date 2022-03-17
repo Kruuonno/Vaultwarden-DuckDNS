f43d5ecef0366b35ecb19e1e230ccaf683debc53
Israel Garcia <81293114+Kruuonno@users.noreply.github.com>
2022-03-16 20:36:14 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,26 +19,10 @@ echo "$domain"
 echo "$email"
 
 
-
 whiptail --ok-button Done --msgbox "Your Info Entered  \nToken: $token  \nDomain: $domain \nEmail: $email " 20 50
 
 cd Vaultwarden-DuckDNS/
 chmod u+x caddy
-
-
-#if (whiptail --title "Do You Want To Start Vaultwarden Docker ?" --yesno "Select Yes or No" 8 78 --no-button "No" --yes-button "Yes" ); then
-#echo "Running, exit status was $?."
-
-#sleep 5s
-
-#echo $(docker-compose up -d);
-
-#else
-#echo "Stopped, exit status was $?."
-
-#fi
-
-#echo "(Exit status was $exitstatus)"
 
 
 sed -i "s/token/${token}/g" ./caddy.env
@@ -51,9 +35,5 @@ sed -i "s/token/${token}/g" ./.env
 
 sed -i "s/domain/${domain}/g" ./.env
 
-
-docker ps
-cat ./caddy.env
-cat ./.env
 chmod u+x ./start-docker.sh
 ./start-docker.sh
