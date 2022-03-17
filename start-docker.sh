4d3fea7f945b9069a61ee57488f281fa82f3fc3b
Israel Garcia <81293114+Kruuonno@users.noreply.github.com>
2022-03-16 20:20:50 -0700

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -1,1 +1,15 @@
 #! /bin/bash
+
+if (whiptail --title "Do You Want To Start Vaultwarden Docker ?" --yesno "Select Yes or No" 8 78 --no-button "No" --yes-button "Yes" ); then
+echo "Running, exit status was $?."
+
+sleep 5s
+
+echo $(docker-compose up -d);
+
+else
+echo "Stopped, exit status was $?."
+
+fi
+
+echo "(Exit status was $exitstatus)"
